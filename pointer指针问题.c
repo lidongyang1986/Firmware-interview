b311dff01fc5089cd82c18a34e7788f65861bf68
lidongyang1986 <lidongyang@ele.uri.edu>
2019-03-08 14:51:19 -0500

    Update pointer指针问题.c

--- pointer指针问题.c
+++ pointer指针问题.c
@@ -1,3 +1,26 @@
 int *ptr; 
 ptr = (int *)0x67a9; 	//地址
 *ptr = 0xaa55;		//赋值
+
+
+
+
+#include <stdio.h>
+
+int main()
+{
+    int a=10;
+    int *p=&a;
+    
+    void *p_v;
+p_v=&a;
+
+void *p_m=&a;	可以	
+*p_v=&a;		错误	
+    
+    printf("Hello World %d\n",*p);
+    printf("p_v %d",*(int *)p_v);
+
+    return 0;
+}
+
